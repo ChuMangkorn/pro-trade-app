66b07960ca2127db1ef2487a1f6c00460b088dc6
Benz - srg <ekkasit.srg@gmail.com>
2025-06-06 14:56:19 +0700

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+// interface TradeTopBarProps {
+//   symbol: string;
+//   accountId: string;
+//   exchange: string | number; // this is union type it's mean should be string or number
+// }
+
+// const TradeTopBar: React.FC<TradeTopBarProps> = ({symbol}) => {
+const Login = () => {
+  return <>Login page</>
+};
+
+export default Login;
